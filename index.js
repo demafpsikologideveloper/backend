460dcd9d778f29cf4aa51b3e3df4cbc0f8a05f74
M Fahrianda <mfahrianda@gmail.com>
2019-04-17 14:49:31 +0700

    refactor

--- index.js
+++ index.js
@@ -2,39 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT;
 
 app.use(bodyParser());
-
-const knex = require('knex')({
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'yourpassword',
-    database: process.env.DB_DATABASE || 'yourdatabasename'
-  }
-});
-
-// GET http://localhost:8000/
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.post('/register', async (req, res) => {
-  const user = {
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  };
-  const newUser = await knex.insert(user).from('users');
-  res.send({
-    user: newUser,
-    message: 'user has been created!'
-  });
-});
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Express app is listening on localhost:${port}`);
 });
+
+const root = require('./middlewares/index');
+const users = require('./middlewares/users');
+
+app.get('/', root.hello);
+// app.get('/users/', users.allUsers);
+app.post('/users/register', users.register);
